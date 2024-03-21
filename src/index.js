96474258b757091658efc8869701a03e31d7c60b
Yitzy Holtzberg <yitzyh18@gmail.com>
2024-03-21 15:52:09 -0400

    It works!

--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
-import { homeButton, menuButton, aboutButton } from "./components"; // Consolidate imports
-import "./style.css";
+import './style.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const content = document.querySelector('#content');
-    const homeBtn = document.querySelector("#home");
-    const aboutBtn = document.querySelector("#about");
-    const menuBtn = document.querySelector("#menu");
-
-    const clearContent = () => content.innerHTML = '';
-
-    content.appendChild(homeButton()); // Initial view
-
-    homeBtn.addEventListener('click', () => {
-        clearContent();
-        content.appendChild(homeButton());
-    });
-
-    menuBtn.addEventListener('click', () => {
-        clearContent();
-        content.appendChild(menuButton());
-    });
-
-    aboutBtn.addEventListener('click', () => {
-        clearContent();
-        content.appendChild(aboutButton());
-    });
+document.getElementById("dropdown").addEventListener("click", function () {
+    document.querySelector(".dropdown-content").classList.toggle("dropdown-content-show");
 });
